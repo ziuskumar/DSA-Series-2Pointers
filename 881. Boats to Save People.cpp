ee389e9349efdb2bb5fb1762f15e22b528af879e
zius kumar <153058154+ziuskumar@users.noreply.github.com>
2026-04-14 09:31:24 +0530

    Add solution for 'Boats to Save People' problem
    Implemented a solution to calculate the minimum number of boats required to carry people based on their weights and a weight limit.

---
+++ 881. Boats to Save People.cpp
@@ -0,0 +1,81 @@
+881. Boats to Save People
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+
+ 
+
+Example 1:
+
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+Example 2:
+
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+
+------------------------------------OPTIMAL---------------------------------
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        sort(people.begin(), people.end());
+        int l = 0, r = people.size()-1;
+
+        int maxboat=0;
+        int currboat=0;
+
+        while(l <= r){
+            if(people[l] + people[r] <= limit){
+                l++;
+                r--;
+            }
+            else{
+                r--;
+            }
+            currboat++;
+            maxboat = max(currboat, maxboat);
+        }
+        return maxboat;
+    }
+};
+
+
+
+
+
+
+--------------------------------BETTER---------------------------------------
+// class Solution {
+// public:
+//     int numRescueBoats(vector<int>& people, int limit) {
+
+//         int n = people.size();
+//         vector<bool> used(n,false);
+//         int boats = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(used[i]) continue;
+
+//             bool paired = false;
+
+//             for(int j=i+1; j<n; j++){
+//                 if(!used[j] && people[i] + people[j] <= limit){
+//                     used[j] = true;
+//                     paired = true;
+//                     break;
+//                 }
+//             }
+//             used[i] = true;
+//             boats++;
+//         }
+//         return boats;
+//     }
+// };
